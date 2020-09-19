5a232bb5b4ddb6e4571390049b8c9a51c2b2fefc
Madhuvantha K.A.N <37581397+Nawod@users.noreply.github.com>
2020-09-20 01:21:06 +0530

    add room1

---
+++ escape_room_ctf/room1.php
@@ -0,0 +1,65 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Escape Room - Room 01</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<body style="background:#1a1a1a">
+    <div class="container">
+    <div class="text-center">
+        <div class="row" style="position:relative;top:40px">
+            <div class="col">
+                <h1 style="color:#ca2f2f;">ROOM 01</h1>
+            </div>
+        </div>
+        <div class="row" style="position:relative;top:50px">
+            <div class="col">
+                <p style="color:#dfdfdf;">"Oh! Now you stucked in a room. To escape from this room, you have to open the padlock."</p>
+                <p style="color:#dfdfdf;">"The passcode of the padlock is inside the safelocker and it has been also locked by a key."</p>
+            </div>
+        </div>
+        <div class="row" style="position:relative;top:90px">
+            <div class="col">
+                    <img src="images/ceaser.jpg" class="img-thumbnail" alt="Responsive image" style="width:auto;height:460px">
+            </div>
+            <div class="col">
+                    <img src="images/safe.jpg" class="img-thumbnail" alt="Responsive image" style="width:400px;height:460px">
+            </div>
+        </div>
+        <hr style="border-top: 2px solid #dfdfdf;position:relative;top:105px">
+        <div class="row" style="position:relative;top:130px">
+            <div class="col">
+                <p style="color:#dfdfdf;">"Unlock the padlock by entering passcode."</p>
+            </div>
+        </div>
+        <div class="row" style="position:relative;top:150px">
+            <div class="col">
+                <div class="text-center">
+                    <img src="images/lock.jpg" class="img-thumbnail" alt="Responsive image" style="width:auto;height:460px">
+                </div>
+            </div>
+        </div>
+        <div class="row" style="position:relative;top:-37px">
+            <div class="col">
+            <form action="" style="width:207px;margin-left:455px;text-align:center">
+                <div class="form-group">
+                    <input type="email" class="form-control" id="LockInput"  placeholder="PASSCODE" style="text-align:center;font-weight:700;color:#ca2f2f">
+                </div>
+                <a href="room1.php"><button type="submit" class="btn btn-outline-danger" style="position:relative;top:50px">Unlock the Door</button></a>
+            </from>
+            </div>
+        </div>
+        <div class="row" style="text-align:center;position:relative;top:125px">
+            <div class="col">
+                <p style="color:#dfdfdf;">"-escape room-"</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
